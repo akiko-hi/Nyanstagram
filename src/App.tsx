ba4e364b6c0391af84df15e543a21d5bd020f762
Akiko <65807177+akiko-hi@users.noreply.github.com>
2020-09-08 13:24:03 +1200

    Add Posts component and basic structure to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './App.scss';
+import Posts from './Posts';
+
 
 function App() {
   return <div className="App">
 
-  </div>
+    <header>
+      <p>Nyanstagram</p>
+      <input />
+      <div>images</div>
+    </header>
+
+    <main>
+      <div className="main_wrapper posts">
+        <div className="videos_section">videos</div>
+
+        <div className="posts_section">
+          <Posts />
+
+          
+        </div>
+
+      </div>
+
+      <div className="main_wrapper">
+        <div>my account</div>
+        <div>suggestions</div>
+      </div>
+    </main>
+  </div >
 }
 
 export default App;
---
+++ src/Posts.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Posts.scss';
+import ProfilePic from './images/mugi.png'
+import EditBtn from './images/edit_btn.png'
+import MugiPost1 from './images/mugi_post1.jpg'
+
+export default function Posts() {
+    return <div className="Post">
+
+        <div className="post_header">
+            <img className="profile_img" src={ProfilePic} alt="profile" />
+            <p>AmazingMugi</p>
+            <img className="edit_post" src={EditBtn} alt="edit" />
+        </div>
+
+        <img className="post" src={MugiPost1} alt="post" />
+
+        <div className="buttons_wrapper">
+            <button className="like_btn"><span role="img" aria-label="like">🤍</span></button>
+            <button><span role="img" aria-label="comment">💭</span></button>
+            <button><span role="img" aria-label="share">🎈</span></button>
+            <button className="favorite_btn"><span role="img" aria-label="favorite">😻</span></button>
+        </div>
+        <div>comments</div>
+        <div className="view_all_comments">View all comments</div>
+        <div className="comment_section">
+            <textarea placeholder="Say meow to this post"></textarea>
+            <button className="post_btn">Post</button>
+        </div>
+    </div>
+}
