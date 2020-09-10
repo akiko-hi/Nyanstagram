87b63dbe486d763c0d530aa078af4e49ea5624a6
Akiko <65807177+akiko-hi@users.noreply.github.com>
2020-09-10 12:14:41 +1200

    Add dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import ProfilePic from './images/mugi.png';
@@ -10,6 +10,9 @@ import Profile from './Profile';
 
 
 function App() {
+
+  const [openDropdownMenu, setOpenDropdownMenu] = useState(false)
+
   return <div className="App">
 
     <header>
@@ -21,11 +24,23 @@ function App() {
       <nav>
         <ul className="nav_images">
           <li><NavLink exact to="/"><HomeIcon /></NavLink></li>
-          <li><NavLink to="/favorite"><FavoriteIcon/></NavLink></li>
-          <li><button><NavLink to="/profile"><img className="profile_img" src={ProfilePic} alt="profile" /></NavLink></button></li>
+          <li><NavLink to="/favorite"><FavoriteIcon /></NavLink></li>
+          <li><button onClick={() => setOpenDropdownMenu(!openDropdownMenu)}>
+              <img className="profile_img" src={ProfilePic} alt="profile" />
+          </button></li>
+          {openDropdownMenu &&
+            <ul className="dropdown_menu">
+              <li><NavLink to="/profile">Profile</NavLink></li>
+              <li>Saved</li>
+              <li>Settings</li>
+              <li>Log Out</li>
+            </ul>
+          }
         </ul>
       </nav>
+
     </header>
+
 
 
     <main>
